2a39c26f00eb06995b448ebcf10129a38edbd311
BrabaxDD <155897950+BrabaxDD@users.noreply.github.com>
2025-02-02 17:33:53 +0100

    basic bugy websocket

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use actix_files::Files;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use std::env;
 use std::path::Path;
 mod host;
 mod player;
@@ -10,22 +8,12 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
             .service(actix_files::Files::new(
                 "/static",
                 Path::new(&format!("./static")),
